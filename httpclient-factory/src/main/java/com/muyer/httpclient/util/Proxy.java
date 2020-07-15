10ab65a5c3c50d0b4ae170374212621b8e2f2d1a
yejiang <1140549742@qq.com>
2020-07-15 23:55:58 +0800

    爬虫工具

---
+++ httpclient-factory/src/main/java/com/muyer/httpclient/util/Proxy.java
@@ -0,0 +1,60 @@
+package com.muyer.httpclient.util;
+
+/**
+ * Description:
+ *  爬虫的代理
+ *
+ * date: 2020/7/15 23:43<br/>
+ *
+ * @author MuyerJ<br />
+ */
+public class Proxy {
+    private String host;
+    private int port;
+    private String userName;
+    private String password;
+
+    public Proxy(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public Proxy(String host, int port, String userName, String password) {
+        this.host = host;
+        this.port = port;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
